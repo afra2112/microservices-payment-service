a1b2d1c0bbd32762f147119c9f3e5c7d5571f3f8
afra65069@gmail.com <andres.ramirez01@globant.com>
2026-03-25 20:50:49 -0500

    new spring version and enable retry annotation

--- src/main/java/org/microservice/paymentservice/PaymentServiceApplication.java
+++ src/main/java/org/microservice/paymentservice/PaymentServiceApplication.java
@@ -3,9 +3,11 @@ package org.microservice.paymentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableRetry
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
